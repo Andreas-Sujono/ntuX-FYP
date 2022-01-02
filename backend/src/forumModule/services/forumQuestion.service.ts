18f10ab45ee94ed54c0c5b6fc9b40cba668a997a
andreas sujono <andr0075@e.ntu.edu.sg>
2022-01-02 22:46:09 +0800

    fix forum endpoint

--- backend/src/forumModule/services/forumQuestion.service.ts
+++ backend/src/forumModule/services/forumQuestion.service.ts
@@ -24,10 +24,10 @@ export class ForumQuestionService extends TypeOrmCrudService<ForumQuestion> {
       left join "forum_answer" on "forum_answer"."questionId" = "forum_question".id
       ${
         !!forumTagId
-          ? `WHERE forum_tag_questions_forum_question."forumTagId" = ${forumTagId}`
-          : ''
+          ? `WHERE forum_tag_questions_forum_question."forumTagId" = ${forumTagId} AND forum_question."parentQuestionId" IS NULL`
+          : 'WHERE forum_question."parentQuestionId" IS NULL'
       }
-      group by forum_question.id, forum_tag_questions_forum_question."forumTagId", "user"."fullName", avatar."imageUrl", "forum_answer"."id"
+      group by forum_question.id, "user"."fullName", avatar."imageUrl"
       ORDER BY forum_question."createdAt" DESC
       LIMIT ${limit} OFFSET ${(page - 1) * limit}
     `);
@@ -43,9 +43,9 @@ export class ForumQuestionService extends TypeOrmCrudService<ForumQuestion> {
       left join "avatar" on "user"."currentAvatarId" = "avatar".id
       left join "forum_answer" on "forum_answer"."questionId" = "forum_question".id
       WHERE forum_question.id NOT IN (
-        SELECT "questionId" FROM forum_answer
-      )
-      group by forum_question.id, forum_tag_questions_forum_question."forumTagId", "user"."fullName", avatar."imageUrl", "forum_answer"."id"
+        SELECT DISTINCT "questionId" FROM forum_answer WHERE "questionId" IS NOT NULL
+      ) AND forum_question."parentQuestionId" IS NULL
+      group by forum_question.id, "user"."fullName", avatar."imageUrl"
       ORDER BY forum_question."createdAt" DESC
       LIMIT ${limit} OFFSET ${(page - 1) * limit}
     `);
@@ -56,6 +56,7 @@ export class ForumQuestionService extends TypeOrmCrudService<ForumQuestion> {
     page = 1,
     limit = 10,
   ): Promise<ForumQuestion[]> {
+    console.log('userId: ', userId);
     return this.repo.query(`
     SELECT forum_question.*, array_agg(forum_tag."name"), "user"."fullName", avatar."imageUrl" as "avatarImageUrl", count(forum_answer."id") as "answerCount"
       FROM forum_question 
@@ -64,8 +65,8 @@ export class ForumQuestionService extends TypeOrmCrudService<ForumQuestion> {
       left join "user" on forum_question."userId" = "user".id
       left join "avatar" on "user"."currentAvatarId" = "avatar".id
       left join "forum_answer" on "forum_answer"."questionId" = "forum_question".id
-      WHERE forum_question."userId" = ${userId}
-      group by forum_question.id, forum_tag_questions_forum_question."forumTagId", "user"."fullName", avatar."imageUrl", "forum_answer"."id"
+      WHERE forum_question."userId" = ${userId} AND forum_question."parentQuestionId" IS NULL
+      group by forum_question.id, "user"."fullName", avatar."imageUrl"
       ORDER BY forum_question."createdAt" DESC
       LIMIT ${limit} OFFSET ${(page - 1) * limit}
     `);
@@ -78,13 +79,17 @@ export class ForumQuestionService extends TypeOrmCrudService<ForumQuestion> {
       },
       relations: [
         'user',
+        'user.currentAvatar',
         'tags',
         'childrenQuestions',
         'answers',
         'childrenQuestions.user',
+        'childrenQuestions.user.currentAvatar',
         'answers.user',
+        'answers.user.currentAvatar',
         'answers.childrenAnswers',
         'answers.childrenAnswers.user',
+        'answers.childrenAnswers.user.currentAvatar',
       ],
     });
   }
